b9fe4357faa42860bc162889ae6940e715e0b188
XYSG <2940133006@qq.com>
2022-08-31 20:54:25 +0800

    提交代码

--- 调试/调试/test.c
+++ 调试/调试/test.c
@@ -1,33 +1,97 @@
 #include<stdio.h>
 
-//struct--结构体关键字   STU--结构体标签   struct STU--结构体类型
-//struct STU
+//int main()
 //{
-//	char name[10];
-//	int age;
-//	char tele[15];
-//	char sex[5];
-//}/*s2*/;//s2是全局变量，尽量少创建全局变量
-typedef struct STU
+//	int arr[10]={0};
+//	int i=0;
+//	for(i=0;i<10;i++)
+//		arr[i]=i;
+//	return 0;
+//}
+
+//调试测试
+//1!+2!+...+n!
+//int main()
+//{
+//	int i=0;
+//	int sum=0;
+//	int n=0;
+//	int ret=1;
+//	scanf("%d",&n);
+//	for(i=1;i<=n;i++)
+//	{
+//		int j=0;
+//		for(j=1;j<=i;j++)
+//			ret*=j;
+//		sum+=ret;
+//		//ret*=i;   //调试后修改成正确代码
+//		//sum+=ret;
+//	}
+//	printf("%d\n",sum);
+//	return 0;
+//}
+
+
+//栈区默认先使用高地址空间
+//数组随着下标的增加，地址是由低到高变化
+//int main()
+//{
+//	int i=0;
+//	int arr[10]={1,2,3,4,5,6,7,8,9,10};
+//	for(i=0;i<=12;i++)
+//	{
+//		printf("sss\n");
+//		arr[i]=0;
+//	}
+//	return 0;
+//}
+
+#include<assert.h>
+
+//char* my_strcpy(char* dest,const char* src)//const防止拷贝对象写反了
+//{
+//	char* ret=dest;
+//	assert(*dest!=NULL);//断言--为假则报错
+//	assert(*src!=NULL);
+//	while(*dest++=*src++)//包含'\0'
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[]="***********";
+//	char arr2[]="bit";
+//	printf("%s\n",my_strcpy(arr1,arr2));
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int a=10;
+//	const int* p=&a;
+//	//const放*左边修饰的是*p--限定了a
+//	//const放*右边修饰的是p--限定了地址，即p不能被改变
+//	*p=20;
+//	return 0;
+//}
+
+//练习规范代码
+//自己写一个strlen
+int my_strlen(const char* str)
 {
-	char name[10];
-	int age;
-	char tele[15];
-	char sex[8];
-}STU;//将结构体重新命名为STU
+	int ret=0;
+	assert(str != NULL);//保证指针有效性
+	while(*str++ !='\0')
+	{
+		ret++;
+	}
+	return ret;
+}
 int main()
 {
-	struct STU s1={"老六",22,"15812546878","男"};//s1--结构体变量，局部变量
-	STU s2={"ss",24,"13546987789","未知"};//改名后就能直接用STU代替struct STU，方便一点
-	STU* ps=&s2;
-	printf("%s\n",s1.name);
-	printf("%d\n",s1.age);
-	printf("%s\n",s1.tele);
-	printf("%s\n",s1.sex);
-
-	printf("%s\n",ps->name);
-	printf("%d\n",ps->age);
-	printf("%s\n",ps->tele);
-	printf("%s\n",ps->sex);
+	char arr[]="asaaxav";
+	printf("字符串长度为%d\n",my_strlen(arr));
 	return 0;
 }
